e44165892f76eb4a64f4860adad0c12c08429dd7
Fangfang Xia <fangfang@cs.uchicago.edu>
2014-12-01 04:27:31 +0000

    minor update

--- service-tests/scripts/test-assembly.sh
+++ service-tests/scripts/test-assembly.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-MODULE_SCRIPTS=DIR/assembly
+MODULE_SCRIPTS=$DIR/assembly
 
-for t in $(MODULE_SCRIPTS) ; do
-    cp $t .
-    if [ $? -ne 0 ] ; then
-        exit 1 ;
-    fi
-    ./$t
-done
+cp $DIR/assembly/arast.t .
+
+./arast.t
 

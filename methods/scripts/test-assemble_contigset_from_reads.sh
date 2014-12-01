7d383867a32fa9911093171c09e627ee0d6b07ed
Fangfang Xia <fangfang@cs.uchicago.edu>
2014-12-01 05:18:35 +0000

    minor update

--- methods/scripts/test-assemble_contigset_from_reads.sh
+++ methods/scripts/test-assemble_contigset_from_reads.sh
@@ -4,4 +4,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cp $DIR/assemble_contigset_from_reads/* .
 
-njs-run-step --params input_ar.json assemble_contigset_from_reads.pl
+njs-run-step --params input_ar.json ./assemble_contigset_from_reads.pl

a15e5637a3d9a8c1354495ad9159b3eacbee5585
Fangfang Xia <fangfang@cs.uchicago.edu>
2014-12-03 17:46:52 +0000

    add communities service

---
+++ service-tests/scripts/test-communities-original.sh
@@ -0,0 +1,12 @@
+
+rsync -avp --del /kb/dev_container/communities_api/test/ test
+
+for f in test/client-tests/*.t; do
+  echo "Testing $f..."
+  perl $f |tee $f.tap
+done
+
+for f in test/script-tests/*.t; do
+  echo "Testing $f..."
+  perl $f |tee $f.tap
+done

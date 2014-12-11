af41e08fdf62dcfa4f4a2680d0d7113eb1711862
Fangfang Xia <fangfang@cs.uchicago.edu>
2014-12-11 23:09:30 +0000

    minor update

--- service-tests/scripts/test-communities-original.sh
+++ service-tests/scripts/test-communities-original.sh
@@ -4,5 +4,6 @@ rsync -avp --del /kb/dev_container/modules/communities_api/test/ test
 for f in test/script-tests/*.t; do
   echo "Testing $f..."
   # first argument is path to test data, second path to output directory (default ./ )
-  perl $f /kb/dev_container/modules/communities_api/test/data/ |tee $f.tap
+  # perl $f /kb/dev_container/modules/communities_api/test/data/ |tee $f.tap
+  prove --timer --formatter=TAP::Formatter::JUnit > $f.junit.xml
 done

b7a82d2f40b08f021f9206d9dcaa7455393d50e4
Fangfang Xia <fangfang@cs.uchicago.edu>
2014-12-11 23:11:53 +0000

    minor update

--- service-tests/scripts/test-communities-original.sh
+++ service-tests/scripts/test-communities-original.sh
@@ -1,7 +1,8 @@
 
 rsync -avp --del /kb/dev_container/modules/communities_api/test/ test
 
-for f in test/script-tests/*.t; do
+# for f in test/script-tests/*.t; do
+for f in test/script-tests/*merge.t; do
   echo "Testing $f..."
   # first argument is path to test data, second path to output directory (default ./ )
   # perl $f /kb/dev_container/modules/communities_api/test/data/ |tee $f.tap

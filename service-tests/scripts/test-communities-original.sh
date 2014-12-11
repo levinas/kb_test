bcc79c637b0c02cb6a1266ada671f82bc8eccc2d
Fangfang Xia <fangfang@cs.uchicago.edu>
2014-12-11 23:20:16 +0000

    minor update

--- service-tests/scripts/test-communities-original.sh
+++ service-tests/scripts/test-communities-original.sh
@@ -6,5 +6,5 @@ for f in test/script-tests/*merge.t; do
   echo "Testing $f..."
   # first argument is path to test data, second path to output directory (default ./ )
   # perl $f /kb/dev_container/modules/communities_api/test/data/ |tee $f.tap
-  prove --timer --formatter=TAP::Formatter::JUnit $f > $f.junit.xml
+  prove --timer --formatter=TAP::Formatter::JUnit $f :: /kb/dev_container/modules/communities_api/test/data/ > $f.junit.xml
 done

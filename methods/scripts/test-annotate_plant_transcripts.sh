f636cf1d72b67590b9f13b946ebe864ab67c27f1
Fangfang Xia <fangfang.xia@gmail.com>
2015-01-14 08:32:36 +0000

    minor update

--- methods/scripts/test-annotate_plant_transcripts.sh
+++ methods/scripts/test-annotate_plant_transcripts.sh
@@ -4,4 +4,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cp $DIR/annotate_plant_transcripts/* .
 
-njs-run-genome-annotation annotate_genome Athaliana.TAIR10.json
+njs-run-genome-annotation annotate_genome Ptrichocarpa.JGI3.0.json

a44eb29348267e7c30cbeadd2357666d395e3a66
Fangfang Xia <fangfang.xia@gmail.com>
2015-01-17 20:22:53 +0000

    minor update

--- apps/scripts/test-genome_assembly.sh
+++ apps/scripts/test-genome_assembly.sh
@@ -2,8 +2,8 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-rsync -avp --del $DIR/[% app %] .
+rsync -avp --del $DIR/genome_assembly .
 
-for f in [% app %]/input*.json; do
+for f in genome_assembly/input*.json; do
   $DIR/../../utils/run_njs_app.pl $f
 done

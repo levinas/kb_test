b17a9d31cad2beb07a1649f0f7e26ff80a83571d
Fangfang Xia <fangfang.xia@gmail.com>
2015-01-17 20:03:04 +0000

    minor update

--- apps/scripts/test-genome_annotation.sh
+++ apps/scripts/test-genome_annotation.sh
@@ -5,5 +5,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rsync -avp --del $DIR/genome_annotation .
 
 for f in genome_annotation/input*.json; do
-  ../../utils/run_njs_app.pl $f
+  $DIR/../../utils/run_njs_app.pl $f
 done

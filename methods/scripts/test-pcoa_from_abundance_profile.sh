c8b5b1f08ca13e7044fc28dc232374ae57eab659
Fangfang Xia <fangfang.xia@gmail.com>
2015-01-22 23:44:57 +0000

    minor update

--- methods/scripts/test-pcoa_from_abundance_profile.sh
+++ methods/scripts/test-pcoa_from_abundance_profile.sh
@@ -5,4 +5,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cp $DIR/pcoa_from_abundance_profile/* .
 
 njs-run-step --param_file input_func_matrix.json --command $KB_TOP/bin/mg-compare-pcoa
+njs-run-step --param_file input_func_matrix_norm.json --command $KB_TOP/bin/mg-compare-pcoa
 

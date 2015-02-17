2480ff3f3714ae5865e0258926ecb7155a5555e2
Fangfang Xia <fangfang.xia@gmail.com>
2015-02-17 21:42:48 +0000

    minor update

--- methods/scripts/test-run_flux_balance_analysis.sh
+++ methods/scripts/test-run_flux_balance_analysis.sh
@@ -4,4 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cp $DIR/run_flux_balance_analysis/* .
 
+export FBA_SERVER_MODE=1
 njs-run-fba-modeling --command runfba --param_file ecoli.model.gf.json

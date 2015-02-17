8814351cebb4249c7692509746f0a6205cb8450e
Fangfang Xia <fangfang.xia@gmail.com>
2015-02-17 21:44:12 +0000

    minor update

--- methods/scripts/test-gapfill_a_metabolic_model.sh
+++ methods/scripts/test-gapfill_a_metabolic_model.sh
@@ -4,4 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cp $DIR/gapfill_a_metabolic_model/* .
 
+export FBA_SERVER_MODE=1
 njs-run-fba-modeling --command gapfill_model --param_file ecoli.model.json

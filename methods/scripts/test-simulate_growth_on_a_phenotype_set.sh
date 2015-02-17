46585ea085aeadf09c8dd36f08ccf5d79d9233d1
Fangfang Xia <fangfang.xia@gmail.com>
2015-02-17 21:44:36 +0000

    minor update

--- methods/scripts/test-simulate_growth_on_a_phenotype_set.sh
+++ methods/scripts/test-simulate_growth_on_a_phenotype_set.sh
@@ -4,4 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cp $DIR/simulate_growth_on_a_phenotype_set/* .
 
+export FBA_SERVER_MODE=1
 njs-run-fba-modeling --command simulate_phenotypes --param_file ecoli.pheno.json
